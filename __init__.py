77b8f87697f94b95430eb12c53bcdef1a8d552c5
Martinits <mamartinitramam@163.com>
2022-01-29 23:42:56 +0800

    use git status

--- __init__.py
+++ __init__.py
@@ -13,21 +13,21 @@ from subprocess import getstatusoutput
 
 ACCEPT_FILE_OLD = ranger.container.directory.accept_file
 
-cache = {}
-
 # Define a new one
 def gitignore_filter(fobj, filters):
     sts = fobj.fm.settings
     if sts.vcs_aware and sts.vcs_backend_git == 'enabled' and not sts.show_hidden:
         dirname, filename = os.path.split(fobj.path)
-        if dirname not in cache:
-            cmd = f'cd {dirname}; git ls-files -z --others --directory --ignored --exclude-standard'
-            retcode, ignored_files = getstatusoutput(cmd)
-            if retcode != 0:
-                cache[dirname] = []
-            else:
-                cache[dirname] = [ os.path.normpath(f) for f in ignored_files.split('\0')[:-1] ]
-        if filename in cache[dirname]:
+        cmd = f'cd {dirname}; git status -s -z --ignored {filename}'
+        retcode, ignored_files = getstatusoutput(cmd)
+        ignored_files = [ os.path.normpath(f) for f in ignored_files.split('\0')[:-1] ]
+        ignored_files = list(filter(lambda x: x.startswith('!!'), ignored_files))
+        if retcode != 0 or len(ignored_files) != 1:
+            return ACCEPT_FILE_OLD(fobj, filters)
+        key = ignored_files[0].split()[-1]
+        if key[-1] == '/':
+            key = key[:-1]
+        if fobj.path.endswith(key):
             return False
     return ACCEPT_FILE_OLD(fobj, filters)
 
